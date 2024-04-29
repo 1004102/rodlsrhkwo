4fe71c34a8f3569b769b7a06dd3a0ed9f43c1da4
윤일영 <dlffhd2@naver.com>
2024-04-29 20:50:31 +0900

    과제 1-2 (1-1 포함)
    1. Scanner를 사용하여 양의 정수 2개(0 포함)를 전달 받을 수 있습니다.
        - 양의 정수는 각각 하나씩 전달 받습니다.
            - 양의 정수는 적합한 타입으로 선언한 변수에 저장합니다.
    
    2. Scanner를 사용하여 사칙연산 기호를 전달 받을 수 있습니다.
        - 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다. (`charAt(0)`)

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,13 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("첫 번째 숫자를 입력하세요: ");
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        int n1 = sc.nextInt();
+        int n1 = Integer.parseInt(sc.nextLine());
         System.out.println("두 번째 숫자를 입력하세요: ");
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        int n2 = sc.nextInt();
+        int n2 = Integer.parseInt(sc.nextLine());
+
+        System.out.println("사칙연산 기호를 입력하세요: ");
+        // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+        char ch = sc.nextLine().charAt(0);
     }
 }
