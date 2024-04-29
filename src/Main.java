39c1ac7de99bbd01f29aff494428a0359da4f907
윤일영 <dlffhd2@naver.com>
2024-04-29 22:47:12 +0900

    과제 1-4
    4. 반복문을 사용하여 반복의 종료를 알려주는 “exit” 문자열을 입력하기 전까지 무한으로 계산을 진행할 수 있도록 소스 코드를 수정합니다.
        - 반복문을 사용합니다. (e.g. for, while …)

--- src/Main.java
+++ src/Main.java
@@ -5,25 +5,36 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("첫 번째 숫자를 입력하세요: ");
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        int n1 = Integer.parseInt(sc.nextLine());
-        System.out.println("두 번째 숫자를 입력하세요: ");
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        int n2 = Integer.parseInt(sc.nextLine());
 
-        System.out.println("사칙연산 기호를 입력하세요: ");
-        // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
-        char ch = sc.nextLine().charAt(0);
+        while (true) {
 
-        int result = 0;
-        /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
-        switch (ch) {
-            case '+' -> result = n1 + n2;
-            case '-' -> result = n1 - n2;
-            case '*' -> result = n1 * n2;
-            case '/' -> result = n1 / n2;
+            System.out.println("첫 번째 숫자를 입력하세요: ");
+            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+            int n1 = Integer.parseInt(sc.nextLine());
+            System.out.println("두 번째 숫자를 입력하세요: ");
+            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+            int n2 = Integer.parseInt(sc.nextLine());
+
+            System.out.println("사칙연산 기호를 입력하세요: ");
+            // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+            char ch = sc.nextLine().charAt(0);
+
+            int result = 0;
+            /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
+            switch (ch) {
+                case '+' -> result = n1 + n2;
+                case '-' -> result = n1 - n2;
+                case '*' -> result = n1 * n2;
+                case '/' -> result = n1 / n2;
+            }
+            System.out.println("결과: " + result);
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            /* exit을 입력 받으면 반복 종료 */
+            String rs = sc.nextLine();
+            if (rs.equals("exit")) {
+                break;
+            }
         }
-        System.out.println("결과: " + result);
     }
 }
