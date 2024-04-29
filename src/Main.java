6ff0de2c91d450e9a4fd437a55199e5f47f173e7
윤일영 <dlffhd2@naver.com>
2024-04-29 23:13:19 +0900

    과제 1-5
    5. 연산 결과 10개를 저장할 수 있는 배열을 선언 및 생성하고 연산의 결과를 저장합니다.
        - 연산의 결과를 저장할 수 있도록 적합한 타입의 배열을 생성합니다.
        - 연산의 결과를 비어있는 곳에 저장하기 위해 저장할 때마다 count 합니다.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,11 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        /* 연산의 결과를 저장할 수 있도록 적합한 타입의 배열을 생성합니다. */
+        /* 연산의 결과가 저장된 배열의 마지막 index를 저장하는 변수를 선언 */
         Scanner sc = new Scanner(System.in);
+        int[] intArray = new int[10];
+        int i = 0;
 
         while (true) {
 
@@ -29,10 +33,11 @@ public class Main {
             }
             System.out.println("결과: " + result);
 
+            intArray[i++] = result;
+
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             /* exit을 입력 받으면 반복 종료 */
-            String rs = sc.nextLine();
-            if (rs.equals("exit")) {
+            if (sc.nextLine().equals("exit")) {
                 break;
             }
         }
